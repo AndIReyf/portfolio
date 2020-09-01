902869cab34b3706085c175d1e6b40b606cb364e
AndIReyf <andrey.prudius@hotmail.com>
2020-09-01 21:05:11 +0200

    Updates

---
+++ precache-manifest.e7c8e058203ebf774d7ac584cf142907.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a295eb7f6cb5471c64744cc8ca1515",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c0d979e343823987812b",
+    "url": "/portfolio/static/css/main.307ffa8f.chunk.css"
+  },
+  {
+    "revision": "a63ff6cd66a7e07975d0",
+    "url": "/portfolio/static/js/2.76cb0641.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.76cb0641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d979e343823987812b",
+    "url": "/portfolio/static/js/main.0122c3a9.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  },
+  {
+    "revision": "c3f042ec3c83958885ec2384061fdf53",
+    "url": "/portfolio/static/media/avatar.c3f042ec.jpeg"
+  },
+  {
+    "revision": "891a71b9a98f1d58491e1b06c4d356a2",
+    "url": "/portfolio/static/media/logo-javascript.891a71b9.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/portfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/portfolio/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "56234232348791710d928f4fc5b6da50",
+    "url": "/portfolio/static/media/skype.56234232.svg"
+  },
+  {
+    "revision": "924c7558f14efee97ebb06cc431f26fa",
+    "url": "/portfolio/static/media/telegram.924c7558.svg"
+  },
+  {
+    "revision": "5fcb9b74a4f26917e3ee7864d973b66e",
+    "url": "/portfolio/static/media/viber.5fcb9b74.svg"
+  },
+  {
+    "revision": "adab28eb901ca70eed29806ac781dfcc",
+    "url": "/portfolio/static/media/whatsapp.adab28eb.svg"
+  },
+  {
+    "revision": "3d856333c3524b6bda85968a6ce3a706",
+    "url": "/portfolio/static/media/work1.3d856333.jpg"
+  },
+  {
+    "revision": "b48116ab4951121f649fb123bb6556a8",
+    "url": "/portfolio/static/media/work2.b48116ab.jpg"
+  }
+]);
